6bcb31a39e2eb20bc628b1586c65723fd2f903db
shenyingge <shenyingge@foxmail.com>
2018-12-15 17:59:25 +0800

    first commit

---
+++ app/src/main/java/com/example/foold/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.foold;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.setContentView(R.layout.login);
+        TextView text_reg = (TextView) findViewById(R.id.newUser);
+        text_reg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+                startActivity(intent);
+                Log.d("MainActivity","intent");
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/foold/SecondActivity.java
@@ -0,0 +1,25 @@
+package com.example.foold;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.setContentView(R.layout.regist);
+        Button back_button = (Button)findViewById(R.id.back_reg);
+        back_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent intent = new Intent(SecondActivity.this,MainActivity.class);
+//                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
